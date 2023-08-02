77e68205bdc1ae478fc0c472acbc902626859ae2
johannesalves <johannes001002@gmail.com>
2023-08-02 17:35:17 -0300

    chore: remove import css from global.ts

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -13,7 +13,6 @@ const GlobalStyles: GlobalStyleComponent<
     GlobalStylesProps,
     DefaultTheme
 > = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600&display=swap');
 
   * {
     margin: 0;
