1e374d41109ee9ad619bc040c538307d8d42d6b9
johannesalves <johannes001002@gmail.com>
2023-08-03 15:18:49 -0300

    feat: component game info

---
+++ src/components/GameInfo/index.stories.tsx
@@ -0,0 +1,21 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import GameInfo from ".";
+import { GameInfoProps } from "./types";
+import mockGame from "./mock";
+
+export default {
+    title: "GameInfo",
+    component: GameInfo,
+    parameters: {
+        backgrounds: {
+            default: "won-dark",
+        },
+    },
+    args: mockGame,
+} as Meta;
+
+export const Default: Story<GameInfoProps> = args => (
+    <div style={{ maxWidth: "144rem", padding: "1.5rem" }}>
+        <GameInfo {...args} />
+    </div>
+);
