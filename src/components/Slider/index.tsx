2aaeb0e0d9bc748a8f8a7437143b8646b92fbad3
johannesalves <johannes001002@gmail.com>
2023-08-08 14:45:46 -0300

    refactor: add slider with forwaredRef function

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,11 +1,19 @@
+import { ForwardRefRenderFunction, forwardRef } from "react";
 import SlickSlider from "react-slick";
 import * as S from "./styles";
 import { SliderProps } from "./types";
 
-export default function Slider({ children, settings }: SliderProps) {
+const Slider: ForwardRefRenderFunction<SlickSlider, SliderProps> = (
+    { children, settings },
+    ref,
+) => {
     return (
         <S.Wrapper>
-            <SlickSlider {...settings}>{children}</SlickSlider>
+            <SlickSlider {...settings} ref={ref}>
+                {children}
+            </SlickSlider>
         </S.Wrapper>
     );
-}
+};
+
+export default forwardRef(Slider);
