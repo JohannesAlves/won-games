1278d11164e26ebfcc8fadd74128ea80fcc0d859
johannesalves <johannes001002@gmail.com>
2023-08-19 19:09:39 -0300

    feat: component orders list

---
+++ src/components/OrdersList/index.stories.tsx
@@ -0,0 +1,19 @@
+import { Story, Meta } from "@storybook/react/types-6-0";
+import OrdersList from ".";
+
+import itemsMock from "./mock";
+import { OrdersListProps } from "./types";
+
+export default {
+    title: "Profile/OrdersList",
+    component: OrdersList,
+    args: {
+        items: itemsMock,
+    },
+} as Meta;
+
+export const Default: Story<OrdersListProps> = args => (
+    <div style={{ maxWidth: 850, margin: "auto" }}>
+        <OrdersList {...args} />
+    </div>
+);
---
+++ src/components/OrdersList/index.tsx
@@ -0,0 +1,25 @@
+import Empty from "components/Empty";
+import GameItem from "components/GameItem";
+import Heading from "components/Heading";
+import * as S from "./styles";
+import { OrdersListProps } from "./types";
+
+const OrdersList = ({ items = [] }: OrdersListProps) => (
+    <S.Wrapper>
+        <Heading lineBottom lineColor="primary" color="black" size="small">
+            My orders
+        </Heading>
+
+        {items.length ? (
+            items.map(item => <GameItem key={item.downloadLink} {...item} />)
+        ) : (
+            <Empty
+                title="You have no orders yet"
+                description="Go back to the store and explore great games and offers"
+                hasLink
+            />
+        )}
+    </S.Wrapper>
+);
+
+export default OrdersList;
