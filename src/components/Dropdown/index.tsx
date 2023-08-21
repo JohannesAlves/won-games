222f4fa040f5c3056ca6db18e48b830821b3bc6f
johannesalves <johannes001002@gmail.com>
2023-08-21 12:31:25 -0300

    feat: dropdown component

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import * as S from "./styles";
+import { DropdownProps } from "./types";
+
+const Dropdown = ({ children, title }: DropdownProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <S.Wrapper isOpen={isOpen}>
+            <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+
+            <S.Content aria-hidden={!isOpen}>{children}</S.Content>
+        </S.Wrapper>
+    );
+};
+
+export default Dropdown;
