a16409df6d83f5cb8428535b9a26f8f5ffa7eb3a
johannesalves <johannes001002@gmail.com>
2023-08-21 12:38:41 -0300

    refactor: add has button prop on cart list component

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -1,15 +1,23 @@
 import GameItem from "components/GameItem";
 import * as S from "./styles";
 import { CartListProps } from "./types";
+import Button from "components/Button";
+import Link from "next/link";
 
-const CartList = ({ items, total }: CartListProps) => (
+const CartList = ({ items, total, hasButton = false }: CartListProps) => (
     <S.Wrapper>
         {items.map((item, index) => (
             <GameItem key={`CartItem-${index}`} {...item} />
         ))}
 
         <S.Footer>
-            Total <S.Total>{total}</S.Total>
+            {!hasButton && <span>Total:</span>}
+            <S.Total>{total}</S.Total>
+            {hasButton && (
+                <Link href="/cart">
+                    <Button>Buy it now</Button>
+                </Link>
+            )}
         </S.Footer>
     </S.Wrapper>
 );
