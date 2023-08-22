9ba2f850e110858798ae553ff6cd243f998604b9
johannesalves <johannes001002@gmail.com>
2023-08-22 14:07:25 -0300

    feat: apollo client & config apollo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,34 @@
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+
 import Head from "next/head";
 import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
+import { useApollo } from "utils/apollo";
 
 function App({ Component, pageProps }: AppProps) {
-    return (
-        <ThemeProvider theme={theme}>
-            <Head>
-                <title>React Avançado - Boilerplate</title>
-                <link rel="shortchurt icon" href="/img/favicon.ico" />
-                <link rel="apple-touch-icon" href="/img/favicon.ico" />
-                <link rel="manifest" href="/manifest.json" />
-                <meta
-                    name="description"
-                    content="A simple project starters to work with ts, react, next js & styled components"
-                />
-            </Head>
+    const client = useApollo(pageProps.initialApoloState);
 
-            <GlobalStyles />
-            <Component {...pageProps} />
-        </ThemeProvider>
+    return (
+        <ApolloProvider client={client}>
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <title>React Avançado - Boilerplate</title>
+                    <link rel="shortchurt icon" href="/img/favicon.ico" />
+                    <link rel="apple-touch-icon" href="/img/favicon.ico" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <meta
+                        name="description"
+                        content="A simple project starters to work with ts, react, next js & styled components"
+                    />
+                </Head>
+
+                <GlobalStyles />
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </ApolloProvider>
     );
 }
 
