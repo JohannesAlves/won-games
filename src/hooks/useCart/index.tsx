c284386cba1a03ab7f4d1df330af947dc9f851d6
johannesalves <johannes001002@gmail.com>
2023-09-10 23:12:37 -0300

    feat: useCart hook

---
+++ src/hooks/useCart/index.tsx
@@ -0,0 +1,88 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { CartContextData, CartProviderProps } from "./types";
+import { getStorageItem, setStorageItem } from "utils/localStorage";
+import { useQueryGames } from "graphql/queries/games";
+import { cartMapper } from "utils/mappers";
+import formatPrice from "utils/formatPrice";
+
+const CART_KEY = "cartItems";
+
+export const CartContextDefaultValues = {
+    items: [],
+    quantity: 0,
+    total: "$0.00",
+    isInCart: () => false,
+    addToCart: () => null,
+    removeFromCart: () => null,
+    clearCart: () => null,
+    loading: false,
+};
+
+export const CartContext = createContext<CartContextData>(CartContextDefaultValues);
+
+const CartProvider = ({ children }: CartProviderProps) => {
+    const [cartItems, setCartItems] = useState<string[]>([]);
+
+    useEffect(() => {
+        const data = getStorageItem(CART_KEY);
+
+        if (data) {
+            setCartItems(data);
+        }
+    }, []);
+
+    const { data, loading } = useQueryGames({
+        skip: !cartItems?.length,
+        variables: {
+            where: {
+                id: cartItems,
+            },
+        },
+    });
+
+    const total = data?.games.reduce((acc, game) => {
+        return acc + game.price;
+    }, 0);
+
+    const isInCart = (id: string) => (id ? cartItems.includes(id) : false);
+
+    const saveCart = (cartItems: string[]) => {
+        setCartItems(cartItems);
+        setStorageItem(CART_KEY, cartItems);
+    };
+
+    const addToCart = (id: string) => {
+        const newCartItems = [...cartItems, id];
+        saveCart(newCartItems);
+    };
+
+    const removeFromCart = (id: string) => {
+        const newCartItems = cartItems.filter((itemId: string) => itemId !== id);
+        saveCart(newCartItems);
+    };
+
+    const clearCart = () => {
+        saveCart([]);
+    };
+
+    return (
+        <CartContext.Provider
+            value={{
+                items: cartMapper(data?.games),
+                quantity: cartItems.length,
+                total: formatPrice(total || 0),
+                isInCart,
+                addToCart,
+                removeFromCart,
+                clearCart,
+                loading,
+            }}
+        >
+            {children}
+        </CartContext.Provider>
+    );
+};
+
+const useCart = () => useContext(CartContext);
+
+export { CartProvider, useCart };
---
+++ src/hooks/useCart/types.ts
@@ -0,0 +1,21 @@
+type CartItem = {
+    id: string;
+    img: string;
+    title: string;
+    price: string;
+};
+
+export type CartContextData = {
+    items: CartItem[] | undefined;
+    quantity: number;
+    total: string;
+    isInCart: (id: string) => boolean;
+    addToCart: (id: string) => void;
+    removeFromCart: (id: string) => void;
+    clearCart: () => void;
+    loading: boolean;
+};
+
+export type CartProviderProps = {
+    children: React.ReactNode;
+};
