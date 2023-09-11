bf08e8fda7264bfb375ba96dd18150de593125e9
johannesalves <johannes001002@gmail.com>
2023-09-11 13:33:36 -0300

    feat: tests utils new render method

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
 import { useApollo } from "utils/apollo";
+import { CartProvider } from "hooks/useCart";
 
 function App({ Component, pageProps }: AppProps) {
     const client = useApollo(pageProps.initialApoloState);
@@ -14,19 +15,21 @@ function App({ Component, pageProps }: AppProps) {
     return (
         <ApolloProvider client={client}>
             <ThemeProvider theme={theme}>
-                <Head>
-                    <title>React Avançado - Boilerplate</title>
-                    <link rel="shortchurt icon" href="/img/favicon.ico" />
-                    <link rel="apple-touch-icon" href="/img/favicon.ico" />
-                    <link rel="manifest" href="/manifest.json" />
-                    <meta
-                        name="description"
-                        content="A simple project starters to work with ts, react, next js & styled components"
-                    />
-                </Head>
+                <CartProvider>
+                    <Head>
+                        <title>React Avançado - Boilerplate</title>
+                        <link rel="shortchurt icon" href="/img/favicon.ico" />
+                        <link rel="apple-touch-icon" href="/img/favicon.ico" />
+                        <link rel="manifest" href="/manifest.json" />
+                        <meta
+                            name="description"
+                            content="A simple project starters to work with ts, react, next js & styled components"
+                        />
+                    </Head>
 
-                <GlobalStyles />
-                <Component {...pageProps} />
+                    <GlobalStyles />
+                    <Component {...pageProps} />
+                </CartProvider>
             </ThemeProvider>
         </ApolloProvider>
     );
