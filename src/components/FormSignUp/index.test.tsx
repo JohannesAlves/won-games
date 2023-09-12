385018615712ac0b2a55f6393d158b0e81f6fb3f
johannesalves <johannes001002@gmail.com>
2023-09-12 15:59:06 -0300

    chore: add mutation register on form sign up and fix test to be a mocked provider

--- src/components/FormSignUp/index.test.tsx
+++ src/components/FormSignUp/index.test.tsx
@@ -2,10 +2,15 @@ import { screen } from "utils/test-utils";
 import { render } from "utils/test-utils";
 
 import FormSignUp from ".";
+import { MockedProvider } from "@apollo/client/testing";
 
 describe("<FormSignUp />", () => {
     it("should render text and link to sign in", () => {
-        render(<FormSignUp />);
+        render(
+            <MockedProvider>
+                <FormSignUp />
+            </MockedProvider>,
+        );
 
         expect(screen.getByRole("link", { name: /sign in/i })).toBeInTheDocument();
         expect(screen.getByText(/already have an account\?/i)).toBeInTheDocument();
--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -5,44 +5,79 @@ import Button from "components/Button";
 import TextField from "components/TextField";
 
 import * as S from "./styles";
+import { useState } from "react";
+import { UsersPermissionsRegisterInput } from "graphql/generated/globalTypes";
+import { useMutation } from "@apollo/client";
+import { MUTATION_REGISTER } from "graphql/mutations/register";
 
-const FormSignUp = () => (
-    <S.Wrapper>
-        <form>
-            <TextField
-                name="name"
-                placeholder="Name"
-                type="name"
-                hasIcon={<AccountCircle />}
-            />
-            <TextField
-                name="email"
-                placeholder="Email"
-                type="email"
-                hasIcon={<Email />}
-            />
-            <TextField
-                name="password"
-                placeholder="Password"
-                type="password"
-                hasIcon={<Lock />}
-            />
-            <TextField
-                name="confirm-password"
-                placeholder="Confirm password"
-                type="password"
-                hasIcon={<Lock />}
-            />
+const FormSignUp = () => {
+    const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+        username: "",
+        password: "",
+        email: "",
+    });
+    const [createUser, { data }] = useMutation(MUTATION_REGISTER);
 
-            <Button size="large" fullWidth>
-                Sign up now
-            </Button>
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
 
-            <S.FormLink>
-                Already have an account? <Link href="/sign-in">Sign in</Link>
-            </S.FormLink>
-        </form>
-    </S.Wrapper>
-);
+        createUser({
+            variables: {
+                input: {
+                    username: values.username,
+                    password: values.password,
+                    email: values.email,
+                },
+            },
+        });
+    };
+
+    const handleInput = (field: string, value: string) => {
+        setValues((previousValues) => ({ ...previousValues, [field]: value }));
+    };
+
+    return (
+        <S.Wrapper>
+            <form onSubmit={handleSubmit}>
+                <TextField
+                    name="username"
+                    placeholder="Username"
+                    type="text"
+                    onInputChange={(value) => handleInput("username", value)}
+                    hasIcon={<AccountCircle />}
+                />
+                <TextField
+                    name="email"
+                    placeholder="Email"
+                    type="email"
+                    onInputChange={(value) => handleInput("email", value)}
+                    hasIcon={<Email />}
+                />
+                <TextField
+                    name="password"
+                    placeholder="Password"
+                    type="password"
+                    onInputChange={(value) => handleInput("password", value)}
+                    hasIcon={<Lock />}
+                />
+                <TextField
+                    name="confirm-password"
+                    placeholder="Confirm password"
+                    onInputChange={(value) => handleInput("confirm-password", value)}
+                    type="password"
+                    hasIcon={<Lock />}
+                />
+
+                <Button size="large" fullWidth>
+                    Sign up now
+                </Button>
+
+                <S.FormLink>
+                    Already have an account? <Link href="/sign-in">Sign in</Link>
+                </S.FormLink>
+            </form>
+        </S.Wrapper>
+    );
+};
 
 export default FormSignUp;
