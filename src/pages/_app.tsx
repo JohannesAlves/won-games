2fa3d1defe97c923f529aac96fb4e7f6c1d5fc83
johannesalves <johannes001002@gmail.com>
2023-09-12 23:35:10 -0300

    feat: next auth config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,46 +1,51 @@
 import NextNProgress from "nextjs-progressbar";
 
-import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import { ApolloProvider } from "@apollo/client";
+import { ThemeProvider } from "styled-components";
 
 import Head from "next/head";
-import { ThemeProvider } from "styled-components";
 
 import GlobalStyles from "styles/global";
 import theme from "styles/theme";
+
 import { useApollo } from "utils/apollo";
 import { CartProvider } from "hooks/useCart";
+
+import type { AppProps } from "next/app";
 
 function App({ Component, pageProps }: AppProps) {
     const client = useApollo(pageProps.initialApoloState);
 
     return (
-        <ApolloProvider client={client}>
-            <ThemeProvider theme={theme}>
-                <CartProvider>
-                    <Head>
-                        <title>React Avançado - Boilerplate</title>
-                        <link rel="shortchurt icon" href="/img/favicon.ico" />
-                        <link rel="apple-touch-icon" href="/img/favicon.ico" />
-                        <link rel="manifest" href="/manifest.json" />
-                        <meta
-                            name="description"
-                            content="A simple project starters to work with ts, react, next js & styled components"
-                        />
-                    </Head>
+        <SessionProvider session={pageProps.session}>
+            <ApolloProvider client={client}>
+                <ThemeProvider theme={theme}>
+                    <CartProvider>
+                        <Head>
+                            <title>React Avançado - Boilerplate</title>
+                            <link rel="shortchurt icon" href="/img/favicon.ico" />
+                            <link rel="apple-touch-icon" href="/img/favicon.ico" />
+                            <link rel="manifest" href="/manifest.json" />
+                            <meta
+                                name="description"
+                                content="A simple project starters to work with ts, react, next js & styled components"
+                            />
+                        </Head>
 
-                    <GlobalStyles />
-                    <NextNProgress
-                        color="#F231A5"
-                        startPosition={0.3}
-                        stopDelayMs={200}
-                        height={2}
-                        showOnShallow={true}
-                    />
-                    <Component {...pageProps} />
-                </CartProvider>
-            </ThemeProvider>
-        </ApolloProvider>
+                        <GlobalStyles />
+                        <NextNProgress
+                            color="#F231A5"
+                            startPosition={0.3}
+                            stopDelayMs={200}
+                            height={2}
+                            showOnShallow={true}
+                        />
+                        <Component {...pageProps} />
+                    </CartProvider>
+                </ThemeProvider>
+            </ApolloProvider>
+        </SessionProvider>
     );
 }
 
---
+++ src/pages/api/auth/[...nextAuth].ts
@@ -0,0 +1,62 @@
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+type GenericObject = {
+    [key: string]: any;
+};
+
+const options: AuthOptions = {
+    pages: {
+        signIn: "/sign-in",
+    },
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {},
+            async authorize(credentials) {
+                const { email, password } = credentials as {
+                    email: string;
+                    password: string;
+                };
+
+                const response = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/auth/local`,
+                    {
+                        method: "POST",
+                        body: new URLSearchParams({ identifier: email, password }),
+                    },
+                );
+
+                const data = await response.json();
+
+                if (data.user) {
+                    return { ...data.user, jwt: data.jwt };
+                } else {
+                    return null;
+                }
+            },
+        }),
+    ],
+    callbacks: {
+        async session({ session, user }: GenericObject) {
+            // Send properties to the client, like an access_token and user id from a provider.
+            session.jwt = user.jwt;
+            session.id = user.id;
+
+            return Promise.resolve(session);
+        },
+
+        async jwt({ token, user }: GenericObject) {
+            if (user) {
+                token.id = user.id;
+                token.email = user.email;
+                token.name = user.username;
+                token.jwt = user.jwt;
+            }
+
+            return Promise.resolve(token);
+        },
+    },
+};
+
+export default NextAuth(options);
