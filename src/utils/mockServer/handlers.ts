70bad04e32584686b72a1084dc718166ba569281
johannesalves <johannes001002@gmail.com>
2023-09-20 21:29:10 -0300

    chore: add handler forgot pass

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,3 +1,38 @@
 import { rest } from "msw";
+import { LoginReqBody } from "./types";
 
-export const handlers = [];
+export const handlers = [
+    rest.post<LoginReqBody>(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+        (request, response, context) => {
+            const { email } = request.body;
+
+            //when got error
+            if (email === "false@gmail.com") {
+                return response(
+                    context.status(400),
+                    context.json({
+                        error: "Bad Request",
+                        message: [
+                            {
+                                messages: [
+                                    {
+                                        message: "This email does not exist",
+                                    },
+                                ],
+                            },
+                        ],
+                    }),
+                );
+            }
+            // when got success
+
+            return response(
+                context.status(200),
+                context.json({
+                    ok: true,
+                }),
+            );
+        },
+    ),
+];
---
+++ src/utils/mockServer/types.ts
@@ -0,0 +1,3 @@
+export type LoginReqBody = {
+    email: string;
+};
