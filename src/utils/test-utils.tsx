8956192a5a0952751732579ce0219f3ec788ec4b
johannesalves <johannes001002@gmail.com>
2023-09-22 16:24:06 -0300

    chore: add context wishlist provider on tests utils

--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,24 +1,34 @@
 import { RenderOptions, render } from "@testing-library/react";
+
+import { WishlistContext, WishlistContextDefaultValues } from "hooks/useWishlist";
+import { WishlistContextData } from "hooks/useWishlist/types";
 import { CartContext, CartContextDefaultValues } from "hooks/useCart";
 import { CartContextData } from "hooks/useCart/types";
+
 import { ReactElement } from "react";
 import { ThemeProvider } from "styled-components";
 import theme from "styles/theme";
 
 type CustomRenderProps = {
     cartProviderProps?: CartContextData;
+    wishlistProviderProps?: WishlistContextData;
 } & Omit<RenderOptions, "queries">;
 
 const customRender = (
     ui: ReactElement,
     {
         cartProviderProps = CartContextDefaultValues,
+        wishlistProviderProps = WishlistContextDefaultValues,
         ...renderOptions
     }: CustomRenderProps = {},
 ) =>
     render(
         <ThemeProvider theme={theme}>
-            <CartContext.Provider value={cartProviderProps}>{ui}</CartContext.Provider>
+            <CartContext.Provider value={cartProviderProps}>
+                <WishlistContext.Provider value={wishlistProviderProps}>
+                    {ui}
+                </WishlistContext.Provider>
+            </CartContext.Provider>
         </ThemeProvider>,
         renderOptions,
     );
